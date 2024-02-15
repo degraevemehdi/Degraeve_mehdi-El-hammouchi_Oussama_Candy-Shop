01481323119d5e4759c48a8f527140195b6a7db9
oussama elhammouchi <mavollo>
2024-02-15 22:21:00 +0100

    added mens & womens clothing category

--- src/pages/CategoryPage.jsx
+++ src/pages/CategoryPage.jsx
@@ -1,16 +1,44 @@
-
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function CategoryPage() {
-    const { categoryName } = useParams(); 
+  const { categoryName } = useParams();
+  const [products, setProducts] = useState([]);
 
-    return (
+  useEffect(() => {
+    axios
+      .get(
+        `https://fakestoreapi.com/products/category/${encodeURIComponent(
+          categoryName
+        )}`
+      )
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data: ", error);
+      });
+  }, [categoryName]);
+
+  return (
     <div>
-        <h2>Catégorie : {categoryName}</h2>
-        
+      <h2>Catégorie : {decodeURIComponent(categoryName)}</h2>
+      <div>
+        {products.map((product) => (
+          <div key={product.id}>
+            <img
+              src={product.image}
+              alt={product.title}
+              style={{ width: "100px", height: "auto" }}
+            />
+            <p>{product.title}</p>
+            <p>${product.price}</p>
+          </div>
+        ))}
+      </div>
     </div>
-    );
+  );
 }
 
 export default CategoryPage;
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,80 +1,88 @@
-import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import React, { useEffect, useState } from 'react';
-import './Homepage.css'
-import axios from 'axios';
-import Slider from 'react-slick';
+import "./Homepage.css";
+import axios from "axios";
+import Slider from "react-slick";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 function Homepage() {
-    const[products, setProducts] = useState([]);
-    useEffect(()=>{
-            axios.get('https://fakestoreapi.com/products')
-        .then(response =>{
-            const data = response.data 
-            console.log(response);
-            const randomProducts = data.sort(() => 0.5 - Math.random()).slice(0, 4);
-            setProducts(randomProducts);
-            console.log(` liste produits : ${products}`);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        const data = response.data;
+        console.log(response);
+        const randomProducts = data.sort(() => 0.5 - Math.random()).slice(0, 4);
+        setProducts(randomProducts);
+        console.log(` liste produits : ${products}`);
+      })
+      .catch((error) => {
+        console.log(`Error : ${error}`);
+      });
+  }, []);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-        })
-        .catch(error => {
-            console.log(`Error : ${error}`);
-        })
-    },[]);
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024, 
-                settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-                }
-            },
-            {
-                breakpoint: 600, 
-                settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-                }
-            },
-            {
-                breakpoint: 480, 
-                settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-                }
-            }
-            ]
-        };
-        
-        return (
-            <div>
-                <nav>
-                    <ul>
-                        <li><Link to="/category/books">Livres</Link></li>
-                        <li><Link to="/category/electronics">Électronique</Link></li>
-                    </ul>
-                </nav>
-                <h2>Featured Products</h2>
-                <Slider {...settings}>
-                    {products.map(product => (
-                    <div className="carrousel" key={product.id}>
-                        <img src={product.image} alt={product.title} />
-                        <h3>{product.title}</h3>
-                        <p>${product.price}</p>
-                    </div>
-                    ))}
-                </Slider>
-            </div>
-        );
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/category/men's clothing">
+              <p>Men&apos;s clothing</p>
+            </Link>
+          </li>
+          <li>
+            <Link to="/category/women's clothing">
+              <p>Women&apos;s clothing</p>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <h2>Featured Products</h2>
+      <Slider {...settings}>
+        {products.map((product) => (
+          <div className="carrousel" key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h3>{product.title}</h3>
+            <p>${product.price}</p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
